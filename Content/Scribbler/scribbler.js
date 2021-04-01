c16eb0db9c8a5ef09eb9f4e39f53b9a8bf3d3b82
Sao Sai Woon Leng <sai.caesar.leng16@gmail.com>
2021-04-01 12:15:39 +0800

    removed scribbler blank spaces

--- Content/Scribbler/scribbler.js
+++ Content/Scribbler/scribbler.js
@@ -26,10 +26,10 @@ jQuery(document).ready(function ($) {
 if (document.getElementsByClassName('demo').length > 0) {
   var i = 0;
     var txt = `
-    Freelance IT Engineer with internship experience at SSMC, currently developing 
-    my portfolio website 💻 and building Gaming PCs 🎮. 
+Freelance IT Engineer with internship experience at SSMC, currently developing 
+my portfolio website 💻 and building Gaming PCs 🎮. 
 
-    ✔️ Accomplished 4 Web Applications 💻 and 3 Electronic Projects 🤖.
+✔️ Accomplished 4 Web Applications 💻 and 3 Electronic Projects 🤖.
     `;
   var speed = 60;
 
